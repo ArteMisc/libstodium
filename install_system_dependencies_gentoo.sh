852780cf99f5cc047e07e63e93423ecd0c3ab642
Daniel Persson <mailto.woden@gmail.com>
2018-01-26 07:25:24 +0100

    Added dependencies setup for gentoo.

---
+++ install_system_dependencies_gentoo.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# Currently only for Gentoo
+echo "Password for: emerge git libtool automake ruby libpcre maven-bin oracle-jdk-bin"
+sudo emerge git libtool automake ruby libpcre maven-bin oracle-jdk-bin
